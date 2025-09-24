83d3dfaac33dc5c2915c27fb93c5193c78eb2404
user <user@WIN-VIGGFF8NEG9>
2025-09-24 13:41:57 +0900

    add:Case03.java,Case04.java
    Case01.java 受講生ログイン機能テストコード追加
    WebElementを利用してIDとパスワード、タイトルボタンの値を取得し、コース詳細画面に遷移するかのテストを行った。
    期待値は以下の通り
    ID：StudentAA01
    パスワード：StudentAA0123
    ログインボタン：ログイン
    
    Case04.java よくある質問画面テストコード追加
    WebElementを利用してタイトルの値を取得し、よくある質問画面に別タブで遷移するかのテストを行った。
    期待値は以下の通り
    タイトル：よくある質問 | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +43,9 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		assertEquals("http://localhost:8080/lms/",webDriver.getCurrentUrl());
+		WebDriverUtils.getEvidence(new Object(){});
 	}
 
 	@Test
@@ -43,6 +53,16 @@ public class Case03 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement id = webDriver.findElement(By.name("loginId"));
+		id.clear(); 
+		id.sendKeys("StudentAA01");
+		final WebElement pass = webDriver.findElement(By.name("password"));
+		pass.clear(); 
+		pass.sendKeys("StudentAA0123");
+		final WebElement login = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[3]/div/input"));
+		login.click();
+		assertThat("コース詳細 | LMS",is(webDriver.getTitle()));
+		WebDriverUtils.getEvidence(new Object(){});
 	}
 
 }
